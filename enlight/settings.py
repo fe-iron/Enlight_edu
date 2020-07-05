8e1b5d5f66095926a33ea2ea050bda06269c36b0
fe-iron <elahifaiz00@gmail.com>
2020-07-05 14:36:38 +0530

    chnage in setting.py file

--- enlight/settings.py
+++ enlight/settings.py
@@ -78,11 +78,8 @@ WSGI_APPLICATION = 'enlight.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'enlight',
-        'USER': 'postgres',
-        'PASSWORD': '12345',
-        'HOST': 'localhost'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
